ce188691557c40fd9c71caba159de6e93531365a
Souvlaki42 <souvlaki420@gmail.com>
2024-07-29 03:56:52 +0300

    Fix api fetching 3

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -11,10 +11,11 @@ export function getBaseUrl() {
 	const production = process.env.NODE_ENV === "production";
 	const test = process.env.NODE_ENV === "test";
 
-	if (production || test) {
-		return process.env.NEXT_PUBLIC_VERCEL_URL ?? "https://diagoras-ikarias.vercel.app";
+	if (test) {
+		return process.env.NEXT_PUBLIC_VERCEL_URL;
+	} else if (production) {
+		return process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL;
 	}
-
 	return "http://localhost:3000";
 }
 
@@ -25,7 +26,7 @@ export const getFacebookFeed = async (after?: string) => {
 		const feed = await ky
 			.get("feed", {
 				searchParams: { after: after ?? "" },
-				prefixUrl: `diagoras-ikarias.vercel.app/api`
+				prefixUrl: `${getBaseUrl()}/api`
 			})
 			.json<FacebookPageFeed>();
 		return feed;
